3dbb545a2b9a565b67f72c2665f673a00ce23e00
Kevin Hanna <github@kdh.codes>
2026-03-19 23:18:11 +0000

    fix(build): link SimulcastEncoderAdapter for video encoding support
    BuiltinVideoEncoderFactory::CreateVideoEncoder() constructs a
    SimulcastEncoderAdapter (builtin_video_encoder_factory.cc:45), but
    librtc_simulcast_encoder_adapter.a was never linked into the final
    binary. The constructor symbol resolved to 0x0, causing a segfault
    on the EncoderQueue when sending video frames through a negotiated
    peer connection.
    
    Found via `nm -g wrtc.node | grep "U "` which showed a single
    undefined webrtc symbol: SimulcastEncoderAdapterC1.

--- scripts/build-webrtc.sh
+++ scripts/build-webrtc.sh
@@ -6,6 +6,6 @@ set -v
 # We want to use system ninja, _NOT_ depot_tools ninja, actually
 export PATH="${DEPOT_TOOLS}/python-bin:${PATH}:${DEPOT_TOOLS}"
 
-export TARGETS="webrtc libjingle_peerconnection libc++ libc++abi builtin_video_encoder_factory builtin_video_decoder_factory rtc_internal_video_codecs"
+export TARGETS="webrtc libjingle_peerconnection libc++ libc++abi builtin_video_encoder_factory builtin_video_decoder_factory rtc_internal_video_codecs rtc_simulcast_encoder_adapter"
 
 ninja $TARGETS
